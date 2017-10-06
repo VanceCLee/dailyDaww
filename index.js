b94c0bde5934d091803f1e9ecbbd4b6d7a71fcf6
Tevis Dryden <TevisDryden@gmail.com>
2017-10-05 20:14:45 -0600

    cool clicks

--- index.js
+++ index.js
@@ -25,4 +25,17 @@ $(document).ready(function() {
 				})
 		}, 2000)
 	});
+	$('img').click(function() {
+		$(this).hide();
+		$.ajax({
+			url : myurl,
+			crossDomain: true,
+			dataType : "json",
+			context: $(this),
+			success : function(parsed_json) {
+			  this.attr("src", parsed_json.message);
+			}
+		})
+	})
 });
+
